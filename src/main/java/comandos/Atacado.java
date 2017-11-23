3212ccf14824ca6a8082e7f7fd4331731fcfd986
Javier Monardo <javorengero2000@yahoo.com.ar>
2017-11-23 13:02:45 -0300

    Maquillaje

--- src/main/java/comandos/Atacado.java
+++ src/main/java/comandos/Atacado.java
@@ -1,31 +1,25 @@
 package comandos;
 
+import estados.EstadoBatalla;
+import mensajeria.PaquetePelear;
+
 public class Atacado extends ComandosEscucha{
 	
-	import dominio.Elfo;
-	import dominio.Humano;
-	import dominio.Orco;
-	import dominio.Personaje;
-	import estados.EstadoBatalla;
-	import mensajeria.PaquetePelear;
-
-	public class Atacado extends ComandosEscucha{
+	@Override
+	public void ejecutar()
+	{
+		PaquetePelear pa = (PaquetePelear)gson.fromJson(cadenaLeida, PaquetePelear.class);
 		
-		@Override
-		public void ejecutar()
-		{
-			PaquetePelear pa = (PaquetePelear)gson.fromJson(cadenaLeida, PaquetePelear.class);
-			
-			juego.getEstadoBatalla().getEnemigo().actualizarAtributos(pa.getPersonajeAtacante(), pa.getCastaAtacante());
-			juego.getEstadoBatalla().getPersonaje().actualizarAtributos(pa.getPersonajeEnemigo(), pa.getCastaEnemigo());
-			
-			EstadoBatalla.paquetePelear.setPersonajeAtacante(pa.getPersonajeEnemigo());
-			EstadoBatalla.paquetePelear.setPersonajeEnemigo(pa.getPersonajeAtacante());
-			EstadoBatalla.paquetePelear.setCastaAtacante(pa.getCastaEnemigo());
-			EstadoBatalla.paquetePelear.setCastaEnemigo(pa.getCastaAtacante());
-					
-			EstadoBatalla.miTurno = true;
-			EstadoBatalla.menuBatalla.setHabilitado(true);
-		}
+		juego.getEstadoBatalla().getEnemigo().actualizarAtributos(pa.getPersonajeAtacante(), pa.getCastaAtacante());
+		juego.getEstadoBatalla().getPersonaje().actualizarAtributos(pa.getPersonajeEnemigo(), pa.getCastaEnemigo());
+		
+		EstadoBatalla.paquetePelear.setPersonajeAtacante(pa.getPersonajeEnemigo());
+		EstadoBatalla.paquetePelear.setPersonajeEnemigo(pa.getPersonajeAtacante());
+		EstadoBatalla.paquetePelear.setCastaAtacante(pa.getCastaEnemigo());
+		EstadoBatalla.paquetePelear.setCastaEnemigo(pa.getCastaAtacante());
+	
+		EstadoBatalla.miTurno = true;
+		EstadoBatalla.menuBatalla.setHabilitado(true);
+	}
 
 }
